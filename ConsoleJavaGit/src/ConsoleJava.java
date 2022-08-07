36e49188d2a63aa1ce55f6e331b2cf708b7f9805
Vipul-Patil <vipulpatil257@gmail.com>
2022-08-07 10:57:48 +0530

    New File To Git

---
+++ ConsoleJavaGit/src/ConsoleJava.java
@@ -0,0 +1,21 @@
+
+public class ConsoleJava {
+
+	public void Add()
+	{
+		int x = 10;
+		int y = 20;
+		
+		int z = x + y;
+		System.out.println("The Addition is :" +z);
+	}
+	public static void main(String[] args) {
+		
+		ConsoleJava cj = new ConsoleJava();
+		cj.Add();
+	
+		System.out.println("Vipul Patil");
+
+	}
+
+}
